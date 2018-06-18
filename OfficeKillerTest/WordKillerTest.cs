56ea0ddef34d1b35ac35453e248354443a1f0715
Joshua Villing <jsvilling@gmail.com>
2018-06-18 11:03:16 +0200

    test lineout for wordkiller

---
+++ OfficeKillerTest/WordKillerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OfficeKillerTest
+{
+    [TestClass]
+    public class WordKillerTest
+    {
+        [TestMethod]
+        public void TestWordKiller_HappyPath()
+        {
+            // Given
+            // spawn word process
+            // init wordkiller
+
+            // When
+            // invoke wordkiller.Kill()
+
+            // Then
+            // assert that no more word processes are running
+        }
+    }
+}
