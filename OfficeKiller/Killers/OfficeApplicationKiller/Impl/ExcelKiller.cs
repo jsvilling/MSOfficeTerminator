8de43eed7687e7c897ef953d388030e0d8bb3fa2
Joshua Villing <jsvilling@gmail.com>
2018-06-18 10:59:29 +0200

    add basic structure for appKillers

---
+++ OfficeKiller/Killers/OfficeApplicationKiller/Impl/ExcelKiller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeKiller.Killers.OfficeApplicationKiller
+{
+    class ExcelKiller : OfficeApplicationKiller
+    {
+        public void Kill()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
